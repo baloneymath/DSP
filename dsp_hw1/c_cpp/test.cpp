46cdcc142e4ce96a610fc243a58339371e924129
chi777 <baloneymath@gmail.com>
2016-03-27 01:47:01 +0800

    roughly finish test.cpp & start to debug

--- dsp_hw1/c_cpp/test.cpp
+++ dsp_hw1/c_cpp/test.cpp
@@ -4,6 +4,7 @@
 
 #include "hmm.h"
 
+using namespace std;
 
 int main(int argc, char* argv[])
 {
@@ -19,12 +20,12 @@ int main(int argc, char* argv[])
   load_models(model_list, hmms, 5);
 
   // start testing
-  while (1) {
-    char o[MAX_SEQ+1];
-    if (fscanf(fptest, "%s", &o[1]) == EOF) break;
-    int T = strlen(&o[1]);
+  char o[MAX_SEQ+1]; // line buffer
+  int T = 0;
+  while (fscanf(fptest, "%s", &o[1]) == 1) {
+    T = strlen(&o[1]);
 
-    double delta[5][MAX_SEQ][MAX_STATE] = {0.0};
+    double delta_log[5][MAX_SEQ][MAX_STATE] = {0.0};
     for (int _i = 0; _i < 5; ++_i) {
       // alias variables
       int N = hmms[_i].state_num;
@@ -33,14 +34,14 @@ int main(int argc, char* argv[])
       auto &a = hmms[_i].transition;
 
       for (int i = 0; i < N; ++i) // initialize delta
-        delta[_i][1][i] = pi[i] * b[o[1] - 'A'][i];
+        delta_log[_i][1][i] = log(pi[i]) + log( b[o[1] - 'A'][i]);
       for (int t = 2; t <= T; ++t) { // create delta table
         for (int j = 0; j < N; ++j) {
           double temp = 0.0;
           for (int i = 0; i < N; ++i)
-            if (delta[_i][t-1][i] * a[i][j] > temp)
-              temp = delta[_i][t-1][i] * a[i][j];
-          delta[_i][t][j] = temp * b[o[t] - 'A'][j];
+            if (delta_log[_i][t-1][i] * a[i][j] > temp)
+              temp = delta_log[_i][t-1][i] + log( a[i][j]);
+          delta_log[_i][t][j] = temp + log(b[o[t] - 'A'][j]);
         }
       }
     }
@@ -50,8 +51,8 @@ int main(int argc, char* argv[])
     for (int _i = 0; _i < 5; ++_i) {
       double temp = 0.0;
       for (int i = 0; i < hmms[_i].state_num; ++i)
-        if (delta[_i][T][i] > temp) temp = delta[_i][T][i];
-      termination[_i] = temp;
+        if (delta_log[_i][T][i] > temp) temp = delta_log[_i][T][i];
+      termination[_i] = exp(temp);
     }
 
     size_t resultNum = 0;
@@ -61,7 +62,6 @@ int main(int argc, char* argv[])
     fprintf(fp, "%lf\n", termination[resultNum]);
 
   }
-
   dump_models(hmms, 5);
 
   return 0;
