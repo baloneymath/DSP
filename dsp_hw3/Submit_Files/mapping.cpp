ec83effc0b40a825f0fb74ba49bae453f2319033
chi777 <baloneymath@gmail.com>
2016-05-21 01:28:12 +0800

    finish mapping

---
+++ dsp_hw3/Submit_Files/mapping.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <fstream>
+#include <cassert>
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+
+using namespace std;
+
+char HI(int wc)
+{
+  return (wc >> 8) & 255;
+}
+
+char LO(int wc)
+{
+  return wc & 255;
+}
+
+int main(int argc, char* argv[])
+{
+  assert(argc == 3);
+  char* output = argv[2];
+  ifstream ifs;
+  ofstream ofs;
+  ifs.open(argv[1], ifstream::in);
+  ofs.open(argv[2], ofstream::out);
+
+  if (!ifs.is_open()) cerr << "Error opening file!!\n";
+
+  map<int, vector<int>> target;
+  // mapping
+  string buf;
+  while (getline(ifs, buf)) {
+    int pos = 0;
+
+    auto yield1big5 = [&pos, &buf] () -> int {
+      int wc = 0;
+      while (pos < buf.size()) {
+        if (buf[pos] != ' ' && buf[pos] != '\n') break;
+        ++pos;
+      }
+      if (pos >= buf.size()) return 0;
+      switch (buf[pos]) {
+        case '/':
+          wc = '/';
+          break;
+        default:
+          wc = buf[pos] & 255;
+          wc = wc << 8;
+          wc |= buf[pos+1] & 255;
+          ++pos;
+      }
+      ++pos;
+      return wc;
+    };
+
+    int key = yield1big5();
+    while (true) {
+      int hc = yield1big5();
+      target[hc].push_back(key);
+      while (hc = yield1big5()) {
+        if (hc == '/') break;
+      }
+      if (hc == 0) break;
+    }
+  }
+  for (auto it = target.begin(); it != target.end(); ++it) {
+    ofs << HI(it->first) << LO(it->first) << " ";
+    vector<int> &vec = it->second;
+    sort(vec.begin(), vec.end());
+    vec.resize(unique(vec.begin(), vec.end()) - vec.begin());
+    for (auto wc : it->second) {
+      ofs << HI(wc) << LO(wc);
+    }
+    ofs << endl;
+    for (auto wc : it->second) {
+      ofs << HI(wc) << LO(wc) << " " << HI(wc) << LO(wc) << endl;
+    }
+  }
+
+  return 0;
+}
